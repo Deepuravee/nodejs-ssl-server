a2b145156b18649dba90fe63ea04bd91a1e1b3f8
Deepshiga Raveendran <deepshigaravee@gmail.com>
2025-04-18 18:39:42 +0530

    Update app.js
    code changed

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
 const express = require('express');
 const app = express();
-const hostname = '0.0.0.0'; // Your server ip address
+const hostname = '0.0.0.0'; // Listen on all network interfaces
 const port = 3000;
 
 const version = '1.0.0';
 
 app.get('/', (req, res) => {
-    // set response content    
     res.sendFile(__dirname + "/html/index.html"); 
-    console.log(`[Version ${version}]: New request => http://${hostname}:${port}`+req.url);
+    console.log(`[Version ${version}]: New request => http://${hostname}:${port}${req.url}`);
+});
 
-})
-
-app.listen(port, () => {
+app.listen(port, hostname, () => {
     console.log(`[Version ${version}]: Server running at http://${hostname}:${port}/`);
-})
+});
