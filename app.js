f04bca61f01a7e56a36872c351bc3b5886b410db
Deepshiga Raveendran <deepshigaravee@gmail.com>
2025-04-18 18:37:46 +0530

    Update app.js
    code changed

--- app.js
+++ app.js
@@ -1,25 +1,26 @@
+app.listen(port, () => {
+    console.log(`[Version ${version}]: Server running at http://${hostname}:${port}/`);
+})
+
 const express = require('express');
 const app = express();
-const hostname = '0.0.0.0'; // Your server ip address
+const hostname = '0.0.0.0'; // Binds to all network interfaces
 const port = 3000;
 
 const version = '1.0.0';
 
 app.get('/', (req, res) => {
-    // set response content    
-        res.send(`<html>
-                    <body>
-                        <h1 style="color:blue;text-align: center;margin-top: 100px;"> [Version ${version}]: This is AMAZING!!! Like & Subscribe!</h1>
-                        <div style="position: fixed;top: 50%;left: 50%;transform: translate(-50%, -50%)">
-                            <img src="https://picsum.photos/400/400?random=1">
-                        </div>
-                    </body>
-                   </html>`);
- 
-  console.log(`[Version ${version}]: New request => http://${hostname}:${port}`+req.url);
+    res.send(`<html>
+                <body>
+                    <h1 style="color:blue;text-align: center;margin-top: 100px;">[Version ${version}]: This is AMAZING!!! Like & Subscribe!</h1>
+                    <div style="position: fixed;top: 50%;left: 50%;transform: translate(-50%, -50%)">
+                        <img src="https://picsum.photos/400/400?random=1">
+                    </div>
+                </body>
+              </html>`);
+    console.log(`[Version ${version}]: New request => http://${hostname}:${port}${req.url}`);
+});
 
-})
-
-app.listen(port, () => {
+app.listen(port, hostname, () => {
     console.log(`[Version ${version}]: Server running at http://${hostname}:${port}/`);
-})
+});
